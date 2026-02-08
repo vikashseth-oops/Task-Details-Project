f6f5dae868306c6d8329e5917d43d09695f9add0
Sethv4506 <sethv4506@gmail.com>
2026-02-08 13:07:24 +0530

    Initial commit: task-details backend project

---
+++ README.md
@@ -0,0 +1,18 @@
+# Task Details Backend
+
+A simple backend API for managing task details.  
+Currently, data is stored in a local `data/tasks.json` file.
+
+## Features
+- Create a task
+- Get all tasks
+- Update a task
+- Delete a task
+
+## Tech Stack
+- Node.js
+- Express
+- File-based storage (JSON)
+- Future: MongoDB integration
+
+## Project Structure
---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const fs = require('fs');
+
+app.set("view engine" , "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(express.static(path.join(__dirname , "public")));
+
+app.get('/' , function(req , res){
+    fs.readdir(`./files` , function(err , files){
+        res.render("index" , {files : files});
+    });
+});
+
+app.get('/files/:filename' , function(req , res){
+    fs.readFile(path.join(__dirname, "files", req.params.filename) ,"utf-8", function(err , filedata){
+        res.render('show' , {filename : req.params.filename , filedata: filedata});
+    })
+    
+});
+
+app.get('/edit/:filename' , function(req , res){
+        res.render('edit' , {filename : req.params.filename});
+    
+});
+
+app.post('/edit' , function(req , 
+res){
+    fs.rename(`./files/${req.body.previous}` , `./files/${req.body.new}` , function(err){
+        res.redirect("/");
+    })
+});
+
+
+app.post('/create' , function(req , res){
+    console.log(req.body);
+    fs.writeFile(`./files/${req.body.title.split(` `).join('')}.txt` , req.body.details , function(err){
+        res.redirect("/");
+    });
+});
+
+
+app.listen(3000 , ()=>{
+    console.log("Live started");
+});
